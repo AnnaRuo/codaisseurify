bf271a4e16b56743dd505a9428aedef3c7cc3031
Anna <anna.ruotanen@gmail.com>
2017-11-11 22:35:52 +0100

    Display songs per artist in show page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,13 @@
 class SongsController < ApplicationController
 
+  def index
+    @songs = Songs.all
+  end
+
   def show
     @song = Song.find(params[:id])
-
   end
-  
+
   def new
     @song = Song.new
   end
