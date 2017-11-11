5cb99c32543bc4e7cb56e072708711a39d2caa42
Anna <anna.ruotanen@gmail.com>
2017-11-11 13:41:00 +0100

    Generate song model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111122906) do
+ActiveRecord::Schema.define(version: 20171111123901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20171111122906) do
     t.string "name"
     t.string "country"
     t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
+    t.string "album"
+    t.datetime "released"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
